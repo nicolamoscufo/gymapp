36ff1d139a3472be6bd12e81fb0e10f5dee401bd
Nicola Moscufo <nicolamoscufo7@gmail.com>
2026-08-26 20:06:42 +0200

    test: initialize Flutter binding for catalog-backed draft commit

--- test/ai_program_draft_commit_service_test.dart
+++ test/ai_program_draft_commit_service_test.dart
@@ -9,6 +9,8 @@ import 'package:gymapp/models/schedule_version.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
   setUp(() {
     SharedPreferences.setMockInitialValues({});
   });
